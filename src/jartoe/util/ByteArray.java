c7aee966827624165c6d6b65fad7326962d5f9ca
Arto <paragonae@gmail.com>
2013-10-30 23:49:16 +0200

    more of arrays

--- src/jartoe/util/ByteArray.java
+++ src/jartoe/util/ByteArray.java
@@ -29,54 +29,121 @@ public final class ByteArray {
 	}
 
 	public void append(byte[] array) {
-		if (array != null && array.length != 0) {
-			if (firstLength == 0) {
-				first = second;
-				firstLength = second.length;
-			} else if (second.length > 0) {
-				ByteArray first = new ByteArray(this.first, second);
-				this.first = first;
-				firstLength = first.available();
-			}
-			second = array;
+		if (firstLength == 0) {
+			first = second;
+			firstLength = second.length;
+			second = EMPTY;
 		}
+		if (second.length > 0) {
+			ByteArray merge = new ByteArray(first, second);
+			first = merge;
+			firstLength = merge.available();
+			offset = 0;
+		}
+		second = array;
 	}
 
 	public int available() {
-		return firstLength + second.length;
+		return firstLength + second.length - offset;
+	}
+
+	public int peek() {
+		return nextByte(false);
+	}
+
+	public byte[] peek(byte[] to) {
+		return peek(to, 0, to.length);
+	}
+
+	public byte[] peek(byte[] to, int offset, int length) {
+		return copy(to, offset, length, false);
 	}
 
 	public byte[] peekAll() {
-		return new byte[0];
+		return peek(new byte[available()]);
 	}
 
 	public int read() {
-		if (offset < firstLength)
-			return readFromFirst();
-		if (offset < second.length) {
-			int val = second[offset];
-			if (++offset == second.length) {
-				second = EMPTY;
-				offset = 0;
-			}
-			return val;
-		}
-		return -1;
+		return nextByte(true);
 	}
 
-	private int readFromFirst() {
-		int val;
-		if (first instanceof ByteArray) {
-			val = ((ByteArray) first).read();
-		} else {
-			byte[] f = (byte[]) first;
-			val = f[offset];
+	public byte[] read(byte[] to) {
+		return read(to, 0, to.length);
+	}
+
+	public byte[] read(byte[] to, int offset, int length) {
+		return copy(to, offset, length, true);
+	}
+
+	public byte[] readAll() {
+		byte[] all = new byte[available()];
+		return read(all);
+	}
+
+	private byte[] copy(byte[] to, int offset, int length, boolean read) {
+		int len = Math.min(length, available());
+		int firstBytes = 0;
+		if (len > 0 && firstLength > 0) {
+			firstBytes = Math.min(len, firstLength);
+			if (first instanceof ByteArray)
+				((ByteArray) first).copy(to, offset, firstBytes, read);
+			else
+				System.arraycopy(first, this.offset, to, offset, firstBytes);
 		}
-		if (++offset == firstLength) {
-			first = EMPTY;
-			offset = 0;
-			firstLength = 0;
+		if (len - firstBytes > 0)
+			System.arraycopy(second, firstLength > 0 ? 0 : this.offset, to, offset + firstBytes, len - firstBytes);
+		if (read)
+			inc(len);
+		return to;
+	}
+
+	private int fromFirst(boolean read) {
+		if (first instanceof ByteArray) {
+			ByteArray array = (ByteArray) first;
+			if (read)
+				return array.read();
+			return array.peek();
+		}
+		return toByte(((byte[]) first)[offset]);
+	}
+
+	private void inc() {
+		inc(1);
+	}
+
+	private void inc(int by) {
+		for (int i = by; --i >= 0;) {
+			if (offset < firstLength) {
+				if (++offset >= firstLength) {
+					offset = 0;
+					first = EMPTY;
+					firstLength = 0;
+				}
+			} else if (offset < second.length) {
+				if (++offset == second.length) {
+					offset = 0;
+					second = EMPTY;
+				}
+			} else
+				break;
+		}
+	}
+
+	private int nextByte(boolean read) {
+		int val = -1;
+		if (offset < firstLength) {
+			val = fromFirst(read);
+			if (read)
+				inc();
+		} else if (offset < second.length) {
+			val = toByte(second[offset]);
+			if (read)
+				inc();
 		}
 		return val;
+	}
+
+	private int toByte(int b) {
+		return 0xff & b;
 	}
 }
