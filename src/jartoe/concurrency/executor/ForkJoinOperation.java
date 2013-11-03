1d458974114861804ce85587ab8aa95e4296cd83
Arto <paragonae@gmail.com>
2013-11-03 21:57:09 +0200

    still missing the meat for the pool

--- src/jartoe/concurrency/executor/ForkJoinOperation.java
+++ src/jartoe/concurrency/executor/ForkJoinOperation.java
@@ -1,23 +1,32 @@
 package jartoe.concurrency.executor;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 /**
  * @author Artoe
  */
 public abstract class ForkJoinOperation extends Operation {
 	private final List<Runnable> operations = new ArrayList<>(0);
-	private int operationCount;
 
-	public ForkJoinOperation() {}
+	public ForkJoinOperation(Executor executor) {
+		super(executor);
+	}
+
+	public ForkJoinOperation() {
+		this(null);
+	}
 
 	@Override
 	protected final void doOperation() {
 		init();
 		final List<Runnable> forks = new ArrayList<>(fork());
-		suspend();
-		execute(forks);
+		if (!forks.isEmpty()) {
+			suspend();
+			execute(forks);
+		}
 	}
 
 	protected abstract List<? extends Runnable> fork();
@@ -26,43 +35,39 @@ public abstract class ForkJoinOperation extends Operation {
 
 	/**
 	 * Joining the results of the forked operations.
-	 * 
-	 * @return <code>true</code> for re-forking
 	 */
-	protected abstract boolean join(List<Runnable> operations);
+	protected abstract void join(List<? extends Runnable> operations);
 
 	void handleJoining() {
-		List<Runnable> forks;
+		List<? extends Runnable> forks;
 		synchronized (this) {
-			forks = operations;
+			forks = new ArrayList<>(operations);
+			operations.clear();
 		}
 		Throwable error = null;
-		boolean refork;
+		boolean refork = true;
 		try {
-			refork = join(forks);
+			join(forks);
 		} catch (Throwable ex) {
 			error = ex;
 			refork = false;
 		}
-		if (refork) {
-			forks = new ArrayList<>(fork());
-			execute(forks);
-		} else {
+		forks = Collections.emptyList();
+		if (refork)
+			forks = fork();
+		if (forks.isEmpty()) {
 			resume(error);
+		} else {
+			execute(forks);
 		}
 	}
 
-	private void execute(List<Runnable> forks) {
-		int count = forks.size();
+	private void execute(List<? extends Runnable> forks) {
 		synchronized (this) {
 			operations.clear();
 			operations.addAll(forks);
-			operationCount = count;
 		}
-		if (count == 0)
-			handleJoining();
-
-		GroupOperation o = new GroupOperation(forks);
+		GroupOperation o = new GroupOperation(executor, forks);
 		o.addFollowUp(new Operation() {
 			@Override
 			protected void doOperation() {
--- src/jartoe/concurrency/executor/GroupOperation.java
+++ src/jartoe/concurrency/executor/GroupOperation.java
@@ -3,41 +3,122 @@ package jartoe.concurrency.executor;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 /**
  * @author Artoe
  */
 public final class GroupOperation extends Operation {
-	private static final int MAX_PER_GROUP = 8;
+	private static final int DEFAULT_PER_GROUP = 8;
 
-	private final List<Runnable> operations;
+	private final List<Runnable> commands;
 
-	public GroupOperation(List<Runnable> commands) {
-		this.operations = new ArrayList<>(commands.size());
-		for (Runnable operation : commands) {
-			if (operation != null)
-				this.operations.add(operation);
+	private final Runnable followUp = new Runnable() {
+		public void run() {
+			handleFollowUp();
 		}
+	};
+	private int running;
+
+	public GroupOperation(Executor executor, List<? extends Runnable> commands) {
+		super(executor);
+		this.commands = new ArrayList<>(commands.size());
+		for (Runnable command : commands) {
+			if (command != null)
+				this.commands.add(command);
+		}
+	}
+
+	public GroupOperation(Executor executor, Runnable... commands) {
+		this(executor, Arrays.asList(commands));
+	}
+
+	public GroupOperation(List<? extends Runnable> commands) {
+		this(null, commands);
 	}
 
 	public GroupOperation(Runnable... commands) {
-		this(Arrays.asList(commands));
+		this(null, commands);
 	}
 
 	@Override
-	protected List<? extends Runnable> fork() {
+	protected void doOperation() {
 		synchronized (this) {
-			List<Runnable> ops = operations.size() > GroupOperation.MAX_PER_GROUP ? operations.subList(0,
-					GroupOperation.MAX_PER_GROUP) : operations;
-			List<Runnable> tmp = new ArrayList<>(ops.size());
-			tmp.addAll(ops);
-			ops.clear();
-			return tmp;
+			if (commands.isEmpty())
+				return;
+		}
+		suspend();
+		executeNextBatch();
+	}
+
+	void handleFollowUp() {
+		boolean done, more = false;
+		synchronized (this) {
+			done = --running <= 0;
+			if (done)
+				more = !commands.isEmpty();
+
+		}
+		if (done) {
+			if (more)
+				executeNextBatch();
+			else
+				resume(null);
 		}
 	}
 
-	@Override
-	protected boolean join(List<Runnable> operations) {
-		return !this.operations.isEmpty();
+	private void executeNextBatch() {
+		ThreadPool pool = null;
+		int groupSize = DEFAULT_PER_GROUP;
+		if (executor instanceof ThreadPool) {
+			pool = (ThreadPool) executor;
+			groupSize = pool.getCoreCount();
+		}
+		List<Runnable> list = prepareNextBatch(groupSize);
+		List<Runnable> ops = new ArrayList<>(list.size());
+		for (Runnable op : list)
+			list.add(follow(op));
+		if (pool != null) {
+			pool.executeNoGrouping(ops);
+		} else if (executor instanceof ExtendedExecutor) {
+			((ExtendedExecutor) executor).execute(ops);
+		} else {
+			for (Runnable op : ops)
+				executor.execute(op);
+		}
+	}
+
+	private Runnable follow(Runnable op) {
+		Runnable wrap = op;
+		if (op instanceof Operation) {
+			((Operation) op).addFollowUp(followUp);
+		} else
+			wrap = new OperationWrapper(op);
+		return wrap;
+	}
+
+	private synchronized List<Runnable> prepareNextBatch(int groupSize) {
+		List<Runnable> ops = commands.size() > groupSize ? commands.subList(0, groupSize) : commands;
+		List<Runnable> tmp = new ArrayList<>(ops);
+		ops.clear();
+		running = tmp.size();
+		return tmp;
+	}
+
+	private final class OperationWrapper implements Runnable {
+		private Runnable op;
+
+		OperationWrapper(Runnable op) {
+			this.op = op;
+		}
+
+		public void run() {
+			try {
+				op.run();
+			} finally {
+				handleFollowUp();
+			}
+		}
+
 	}
 }
